0ca928cd796bddcae2fe2772028da316bde8e33c
lenovo <tech.amolw@gmail.com>
2022-06-25 20:47:13 +0530

    comment added

--- src/main/java/com/codesimplify/restservices/restcontroller/UserController.java
+++ src/main/java/com/codesimplify/restservices/restcontroller/UserController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
+	//first welcome message restcontroller method we called
 	@GetMapping("/welcome")
 	public String welcomeUser()
 	{
