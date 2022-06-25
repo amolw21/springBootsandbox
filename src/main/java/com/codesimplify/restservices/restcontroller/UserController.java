5bb6892a1a21558ed00d18fff0c850ac390b21fb
lenovo <tech.amolw@gmail.com>
2022-06-25 21:00:08 +0530

    df

--- src/main/java/com/codesimplify/restservices/restcontroller/UserController.java
+++ src/main/java/com/codesimplify/restservices/restcontroller/UserController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
-	//first welfcome message restcontroller method wse called
+	//first welfcome message restcontroldsler method wse called
 	@GetMapping("/welcome")
 	public String welcomeUser()
 	{
