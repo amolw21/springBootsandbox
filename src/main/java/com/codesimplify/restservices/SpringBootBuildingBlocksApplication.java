70a44e066a747f385dbc72efcde2b1ed74d00625
lenovo <tech.amolw@gmail.com>
2023-01-07 22:45:03 +0530

    basic commit

--- src/main/java/com/codesimplify/restservices/SpringBootBuildingBlocksApplication.java
+++ src/main/java/com/codesimplify/restservices/SpringBootBuildingBlocksApplication.java
@@ -2,7 +2,7 @@ package com.codesimplify.restservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//comment added
+
 @SpringBootApplication
 public class SpringBootBuildingBlocksApplication {
 
